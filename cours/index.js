3aff11281eacee83c65e5197ace1b5526bf04556
Helene Quintard <helene.quintard@lapiscine.pro>
2023-10-19 17:33:03 +0200

    Add explanations about Array and Object

--- cours/index.js
+++ cours/index.js
@@ -76,3 +76,70 @@ if (is30yearsOld){
     // console.log (`Paul n'a pas 30 ans`)
 }
 // console.log(myNb)
+
+const bigNb1 = 500
+const bigNb2 = 400
+
+
+// écrire la condition pour afficher le plus grand nombre
+
+// const resultComparison = bigNb1 > bigNb2
+
+if (bigNb1 > bigNb2) { 
+    // console.log (bigNb1)
+} else if (bigNb1 < bigNb2) {
+    // console.log (bigNb2)
+} else if (bigNb1 == bigNb2) {
+    // console.log (bigNb1, bigNb2)
+}
+// console.log(`Bonjour ${firstname}, je ne connaissais pas ton prénom avant.`) Ecriture plus moderne pas de virgule entre 2 variables
+// console.log ('Bonjour' + firstname + ', je ne connaissais pas ton prénom avant')
+
+// VARIABLE COMPLEXE : LES TABLEAUX (Array)
+let arr1 = [3, 5, 0, 204.5, 'hello', false, 'world']
+
+let arrResult1 = `${arr1[4]} ${arr1[6]}` 
+
+let arrResult2
+// On teste le boleen du tableau
+if ("truthy") { 
+    arrResult2 = `${arr1[4]} ${arr1[6]}`
+} else {
+    arrResult2 = arr1[1] + arr1[3]
+}
+// console.log (arrResult2)
+
+arr1 [4] = "bonjour"
+// console.log(arr1);
+
+//changer la valeur de 204.5 en 190 par soustraction de 14.5
+arr1[3] -= 14.5
+// console.log(arr1);
+
+// VARIABLE COMPLEXE : LES OBJETS 
+let obj1 = {
+    maPremierePropriete: 12,
+    maDeuxiemePropriete: 14,
+    prop3: 11,
+    prop9: 'hello'
+}
+
+console.log(obj1.prop9)
+
+let identity2 = {
+    firstname: 'Pierre',
+    lastname: 'Durand',
+    age: 35,
+    town: 'Talence'
+}
+    /** A l'aide de l'objet précédent, construire la chaîne de caractère ==> Je m'appelle Paul Doazan, j'ai 35 ans et j'habite à Talence.
+     
+     */
+  
+    let resultFromObject = `Je m'appelle ${identity.firstname} ${identity.lastname}, j'ai ${identity.age} ans et j'habite à ${identity.town}.`
+
+    console.log(resultFromObject);
+
+//   Pour ceux qui ont terminé, créer un deuxième objet identity2, stocker identity et identity2 dans un tableau, et écrire la phrase précédente en utilisant le tableau.
+  
+let arrIdentities = [identity, identity2]
